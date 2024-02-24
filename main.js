e6d4842a017f4567771a798b8a1655642e1dfd30
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2024-02-24 23:56:32 +0530

    initial commit

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 const navLinks = document.querySelectorAll("#navLinks ul .link");
 
-// navLinks.onclick = () => {
-//   product.nextElementSibling.classList.toggle("show");
-// };
-
 navLinks.forEach((links) => {
   links.addEventListener("click", (e) => {
     const link = e.currentTarget;
