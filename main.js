062a7b3dcee4f9da59d69e8dd9999045f0362d16
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2024-02-25 19:27:43 +0530

    added some styles content and some logic

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
+const menuBtn = document.querySelectorAll(".menu-btn .menuBtn");
 const navLinks = document.querySelectorAll("#navLinks ul .link");
+
+// menuBtn.forEach((btn) => {
+//   btn.addEventListener("click", (b) => {
+//     b.currentTarget.classList.toggle("hide");
+//   });
+// });
 
 navLinks.forEach((links) => {
   links.addEventListener("click", (e) => {
