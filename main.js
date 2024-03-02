aa57b04f999fcc7d839443d95959a574622708fe
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2024-03-02 17:23:05 +0530

    initial commit

--- main.js
+++ main.js
@@ -1,18 +1,10 @@
-const linksReg = document.querySelector(".links-reg1");
+const linksReg = document.querySelector(".links-reg");
 const openMenu = document.getElementById("hamburgericon");
-
-function ScreenWidth() {
-  if (screen.width < 971 && openMenu.classList.contains("active")) {
-    linksReg.style.display = "block";
-  } else {
-    linksReg.style.display = "none";
-  }
-}
 
 function openMenuOnClick() {
   openMenu.classList.toggle("active");
   if (openMenu.classList.contains("active")) {
-    linksReg.style.display = "block";
+    linksReg.style.display = "flex";
     openMenu.src = "./images/icon-close.svg";
   } else {
     linksReg.style.display = "none";
